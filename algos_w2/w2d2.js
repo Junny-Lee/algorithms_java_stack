b16c7bab39b7384355819058d75b7582dac4b59b
Junny Lee <junny1997@gmail.com>
2021-06-15 09:56:42 -0700

    add algos

--- algos_w2/w2d2.js
+++ algos_w2/w2d2.js
@@ -1,4 +1,3 @@
-// Tuesday
 // Queue
 // FIFO (First in, first out)
 
@@ -14,10 +13,32 @@ class Queue {
     }
 
     // add to the back
-    enqueue(node) {}
+    enqueue(node) {
+        if(this.isEmpty()) {
+            this.front = node;
+            this.back = node;
+        } else {
+            this.back.next = node;
+            this.back = node;
+        }
+    }
 
     // remove from the front
-    dequeue() {}
+    dequeue() {
+        if(this.isEmpty()) {
+            console.log("Queue is empty");
+        }   else if(this.front == this.back) {
+            var toBeReoved = this.front;
+            this.front = null;
+            this.back = null;
+            return toBeRemoved;
+        } else {
+            var toBeRemoved = this.front;
+            this.front = this.front.next;
+            toBeRemoved.next = null;
+            return toBeRemoved;
+        }
+    }
 
     // check the front of the queue
     peek() {
@@ -42,4 +63,12 @@ class Node {
 // return the queue back to it's original order when you are done
 // you are not allowed to linearly traverse the queue
 // only use public methods enqueue, dequeue, peek, isempty
-function readQueue(queue) {}
+function readQueue(queue) {
+    var newQueue = new Queue();
+    while(!queue.isEmpty()) {
+        console.log(queue.peek());
+        newQueue.enqueue(queue.dequeue());
+    }
+    
+    return newQueue;
+}
