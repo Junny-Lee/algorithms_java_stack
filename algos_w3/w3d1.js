0ce4e46700811b17a9bc475e3fa6ed951d6c058e
Junny Lee <junny1997@gmail.com>
2021-06-21 09:35:07 -0700

    another alogs

--- algos_w3/w3d1.js
+++ algos_w3/w3d1.js
@@ -16,12 +16,37 @@ class DLList {
     }
 
     // == Main Methods ==
-
     // push to head
-    addHead(node) {}
+    addHead(node) {
+        if (!this.length){
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.head.prev = node;
+            node.next = this.head;
+            this.head = node;
+        }
+        this.length++;
+    }
 
     // pop from tail
-    removeTail() {}
+    removeTail() {
+        if (!this.length){
+            return null;
+        } else {
+            let nodeToRemove = this.tail;
+            if (this.length === 1){
+                this.head = null;
+                this.tail = null;
+            } else {
+                this.tail = this.tail.prev;
+                this.tail.next = null;
+                nodeToRemove.prev = null;
+            }
+            this.length --;
+            return nodeToRemove;
+        }
+    }
 
     // return is empty
     isEmpty() {
@@ -36,8 +61,44 @@ class DLList {
     // == Bonus Methods, just inverted versions of the first set ==
 
     // push to tail
-    addTail(node) {}
+    addTail(node) {
+        if (!this.length){
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.tail.next = node;
+            node.prev = this.tail;
+            this.tail = node;
+        }
+        this.length++;
+    }
 
     // pop from head
-    removeHead() {}
+    removeHead() {
+        if (!this.length){
+            return null;
+        } else {
+            let nodeToRemove = this.head;
+            if (this.length === 1){
+                this.head = null;
+                this.tail = null;
+            } else {
+                this.head = this.head.next;
+                this.head.prev = null;
+                nodeToRemove.next = null;
+            }
+            this.length --;
+            return nodeToRemove;
+        }
+    }
 }
+
+
+var myDLL = new DLList();
+
+myDLL.addHead(new DLLNode(40));
+myDLL.addHead(new DLLNode(30));
+myDLL.addHead(new DLLNode(20));
+myDLL.addHead(new DLLNode(10));
+console.log("removed node: ", myDLL.removeHead());
+console.log(myDLL);
